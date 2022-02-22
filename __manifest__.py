20e14c4bd55b27a5efe03c6828deb05ff8b85b77
Nizamuddin Mohamed <webnizam@gmail.com>
2022-02-22 14:20:53 +0400

    multi geofence with tags widget, loader on attendance, etc

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,8 @@
     "website": "https://fdtech.ae",
     "depends": [
         "hr_attendance",
-        "hr"],
+        "hr"
+    ],
     "data": [
         "views/assets.xml",
         "security/ir.model.access.csv",
